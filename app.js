c916c729a8574a34431e768d529d85f62a5783ae
Song Shao Ting <qwe820610@gmail.com>
2020-12-28 09:32:31 +0800

    feat: add app.js setting

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyparser = require('body-parser')
+const methodOverride = require('method-override')
+const app = express()
+const port = 3000
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+app.use(methodOverride('_method'))
+app.use(bodyparser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.send('MY web app')
+})
+
+app.listen(port, () => {
+    console.log(`Express is running on http://localhost:${port}`)
+})

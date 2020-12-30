88c51f4d2ad4a65a1ece7aba58ff215db4b0c186
Song Shao Ting <qwe820610@gmail.com>
2020-12-30 14:22:21 +0800

    feat: modify /records/new routes

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -1,14 +1,24 @@
 const express = require('express')
 const router = express.Router()
 const Category = require('../../models/category')
+const Record = require('../../models/record ')
 
 router.get('/new', (req, res) => {
   Category.find()
     .lean()
     .then(categories => res.render('new', { categories }))
 })
-router.post('/', (req, res) => {
-  console.log('req.body', req.body)
+
+router.post('/', async (req, res) => {
+  const record = req.body
+  const list = await Category.find({ title: `${record.category}` })
+  try {
+    record.icon = list[0].icon
+    Record.create(record)
+    return res.redirect('/')
+  } catch (err) {
+    console.log(err)
+  }
 })
 
 module.exports = router

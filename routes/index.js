1e2d33cdb8222f55a14a5809c4bccc8041fc790e
Song Shao Ting <qwe820610@gmail.com>
2021-01-18 14:16:24 +0800

    feat: add use modules

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router()
 const home = require('./modules/home')
 const records = require('./modules/records')
 const search = require('./modules/search')
+const users = require('./modules/user')
 
 router.use('/', home)
 router.use('/records', records)
 router.use('/search', search)
+router.use('/users', users)
 
 module.exports = router

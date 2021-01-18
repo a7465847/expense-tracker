1253530f4de1c754082e115e23581351cc279ae8
Song Shao Ting <qwe820610@gmail.com>
2021-01-18 21:44:59 +0800

    feat: add user authentication to routers

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,11 @@ const home = require('./modules/home')
 const records = require('./modules/records')
 const search = require('./modules/search')
 const users = require('./modules/user')
+const { authenticator } = require('../middleware/auth')
 
-router.use('/', home)
-router.use('/records', records)
-router.use('/search', search)
+router.use('/records', authenticator, records)
+router.use('/search', authenticator, search)
 router.use('/users', users)
+router.use('/', authenticator, home)
 
 module.exports = router

79bb80a970c5d962be13df84938ad4180c2d8945
Song Shao Ting <qwe820610@gmail.com>
2021-01-19 11:27:51 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride = require('method-override')
 const helpers = require('handlebars-helpers')
 const comparison = helpers.comparison()
 const routes = require('./routes')
+const flash = require('connect-flash')
 require('./config/mongoose')
 
 const app = express()
@@ -25,10 +26,13 @@ app.use(methodOverride('_method'))
 app.use(bodyparser.urlencoded({ extended: true }))
 
 usePassport(app)
-
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  res.locals.errors_msg = req.flash('errors_msg')
   next()
 })
 
--- routes/modules/user.js
+++ routes/modules/user.js
@@ -19,10 +19,28 @@ router.get('/register', (req, res) => {
 router.post('/register', async (req, res) => {
   try {
     const { name, email, password, confirmPassword } = req.body
+    const errors = []
     const searchEmail = await User.exists({ email })
-    if (searchEmail) {
-      console.log('重複註冊')
+    if (!name || !email || !password || !confirmPassword) {
+      errors.push({ message: '所有欄位都是必填。' })
+    }
+    if (password !== confirmPassword) {
+      errors.push({ message: '密碼與確認密碼不相符！' })
+    }
+    if (errors.length) {
       return res.render('register', {
+        register: true,
+        errors,
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    }
+    if (searchEmail) {
+      errors.push({ message: '這個 Email 已經註冊過了。' })
+      return res.render('register', {
+        errors,
         name,
         email,
         password,
@@ -43,6 +61,7 @@ router.post('/register', async (req, res) => {
 
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '你已經成功登出。')
   res.redirect('/users/login')
 })
 
